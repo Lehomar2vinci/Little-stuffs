b6c1143b86e11eef0ff958b52715b19508392dfe
Nathan Chambrette ( Lehomar2vinci ) <67152033+Lehomar2vinci@users.noreply.github.com>
2023-01-28 14:58:08 +0100

    Create Couperfoutre.cs

---
+++ Couperfoutre.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Security;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        //fait avec amour par Lehomar2vinci aka Nathan Chambrette
+        static void Main(string[] args)
+        {
+            string name1;
+            string name2;
+
+            Console.WriteLine("Bonjour! \n"
+                + "Veuillez saisir un nom avec son article (ex : Un abruti ou Une lampe )");
+            name1 =Console.ReadLine();
+
+            Console.WriteLine("Bonjour! \n"
+                + "Veuillez saisir un 2° nom avec son article (ex : Une poelle ou Un string )");
+            name2 = Console.ReadLine();
+
+            Console.WriteLine("Le résultat est : \n" 
+                "\n"
+                +"Couper " 
+                + name1 
+                + " foutre sur " 
+                + name2);
+        }
+    }
+}
