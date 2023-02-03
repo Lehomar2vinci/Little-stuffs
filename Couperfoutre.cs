dce96b76694226e37270ebea1bca28d83456b0c4
Nathan Chambrette ( Lehomar2vinci ) <67152033+Lehomar2vinci@users.noreply.github.com>
2023-02-03 15:33:42 +0100

    Rajout de boucle pour recommencer

--- Couperfoutre.cs
+++ Couperfoutre.cs
@@ -11,23 +11,32 @@ namespace MyApp // lol
             string name1;
             string name2;
 
-            Console.WriteLine("Bonjour! \n"
-                + "Veuillez saisir un nom avec son article (ex : Un abruti ou Une lampe )");
-            name1 =Console.ReadLine();
+             while (continueCondition2)
+                    { 
+                Console.WriteLine("Bonjour! \n"
+                     + "Veuillez saisir un nom avec son article (ex : Un abruti ou Une lampe )");
+                name1 =Console.ReadLine();
+
+                Console.WriteLine(" \n"
+                                + "Tu n'est pas débile. Saisis un 2° nom avec son article (ex : Une poelle ou un string ) : ");
+                name2 = Console.ReadLine();
+
+                Console.WriteLine("Le résultat est : \n" 
+                                  "\n"
+                                 +"Couper " 
+                                 + name1 
+                                 + " foutre sur " 
+                                 + name2
+                                 + "");
+                Console.WriteLine("=================================\n"
+                                 + "|  Voulez-vous continuer ? (O/N) |\n"
+                                 + "=================================\n");
+                string reponse2 = Console.ReadLine();
+                continueCondition2 = (reponse2.ToUpper() == "O");
+            }
 
             Console.WriteLine(" \n"
-                + "Tu n'est pas débile. Saisis un 2° nom avec son article (ex : Une poelle ou un string ) : ");
-            name2 = Console.ReadLine();
-
-            Console.WriteLine("Le résultat est : \n" 
-                "\n"
-                +"Couper " 
-                + name1 
-                + " foutre sur " 
-                + name2);
-            
-             Console.WriteLine(" \n"
-                + "Tu est trop fort et c'est la fin du programme. merci d'avoir joué avec ! Au revoir! ");
+                                + "C'est la fin du programme. merci d'avoir joué avec ! Au revoir! ");
         }
     }
 }
