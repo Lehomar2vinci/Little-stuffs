c98e5c8c52df8b2cae8d4e94583a018419679437
Nathan Chambrette ( Lehomar2vinci ) <67152033+Lehomar2vinci@users.noreply.github.com>
2023-02-07 19:01:37 +0100

    Update Couperfoutre.cs

--- Couperfoutre.cs
+++ Couperfoutre.cs
@@ -22,7 +22,7 @@ namespace CouperFoutre // lol
             while (continueCondition1)
             {
 
-                Console.WriteLine("Bienvenu dans le jeu du ");
+                Console.WriteLine("Bienvenu dans le jeu du couper foutre !");
 
                 // liste de nom1
 
