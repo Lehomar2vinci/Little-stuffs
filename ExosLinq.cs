1a5d776ca2c51c602704fba66c4be4393e50a4b7
Nathan Chambrette ( Lehomar2vinci ) <67152033+Lehomar2vinci@users.noreply.github.com>
2023-02-08 11:46:30 +0100

    Create ExosLinq.cs

---
+++ ExosLinq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nombres = { 67, 92, 153, 15 };
+
+            var result = from nombre in nombres
+                         where nombre > 30 && nombre < 100
+                         select nombre;
+            List<int> nombresFiltres = result.ToList();
+
+            foreach (int number in nombresFiltres)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
