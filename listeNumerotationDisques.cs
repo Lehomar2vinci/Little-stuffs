4c7e4c039d35455fcc5afad30d6aabd16757a3bd
Nathan Chambrette ( Lehomar2vinci ) <67152033+Lehomar2vinci@users.noreply.github.com>
2023-02-12 19:14:48 +0100

    Create listeNumerotationDisques.cs

---
+++ listeNumerotationDisques.cs
@@ -0,0 +1,18 @@
+// de quoi faire 49 skeuds 
+
+using System;
+
+namespace cArpeggiator //lol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int jeFaisDesNomsDeSkeud = 49;
+            for (int i = 1; i < jeFaisDesNomsDeSkeud +1 ; i++) 
+            {
+                Console.WriteLine("Voilà mon skeud : " + i );
+            }
+        }
+    }
+}
